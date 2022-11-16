5c79b64a8bd7f46fb94f02f18552235cff4cf688
Martin Durant <martin.durant@utoronto.ca>
2022-11-16 15:46:58 -0500

    latest

--- README.md
+++ README.md
@@ -2,47 +2,54 @@
 Benchmark and run scripts for shared memory on dask
 
 
-### Preliminary results
+Preliminary results
 
 ```
-shm_distributed/test_runs.py::test_workflow[worker_scatter_workflow-pickle] 2.765536s
-Start: 602 resident, 370 unique
-End: 4701 resident, 4471 unique
-Δmem: 4147
+ pickle worker_scatter_workflow
+1.598706s
+Start: 897 resident, 571 unique
+End: 1412 resident, 1086 unique
+Δmem: 538
 
-shm_distributed/test_runs.py::test_workflow[worker_scatter_workflow-lmdb] 3.419665s
-Start: 605 resident, 373 unique
-End: 5727 resident, 1401 unique
-Δmem: 1061
+ lmdb worker_scatter_workflow
+2.835939s
+Start: 899 resident, 572 unique
+End: 5511 resident, 1087 unique
+Δmem: 43
 
-shm_distributed/test_runs.py::test_workflow[worker_scatter_workflow-plasma] 0.867145s
-Start: 1649 resident, 1400 unique
-End: 3700 resident, 3449 unique
-Δmem: 1037
+ plasma worker_scatter_workflow
+1.046257s
+Start: 1431 resident, 1087 unique
+End: 2970 resident, 2624 unique
+Δmem: 530
 
-shm_distributed/test_runs.py::test_workflow[worker_scatter_workflow-vineyard] 0.783219s
-Start: 1651 resident, 1401 unique
-End: 4724 resident, 2426 unique
-Δmem: 1041
+ vineyard worker_scatter_workflow
+0.802621s
+Start: 1431 resident, 1086 unique
+End: 3480 resident, 1087 unique
+Δmem: 18
 
-shm_distributed/test_runs.py::test_workflow[client_scatter_workflow-pickle] 2.114218s
-Start: 1631 resident, 1399 unique
-End: 5729 resident, 5498 unique
-Δmem: 4113
+ pickle client_scatter_workflow
+1.798240s
+Start: 1411 resident, 1084 unique
+End: 4485 resident, 4158 unique
+Δmem: 3084
 
-shm_distributed/test_runs.py::test_workflow[client_scatter_workflow-lmdb] 2.557086s
-Start: 1634 resident, 1400 unique
-End: 6757 resident, 1403 unique
-Δmem: 15
+ lmdb client_scatter_workflow
+1.399181s
+Start: 1924 resident, 1596 unique
+End: 5509 resident, 1597 unique
+Δmem: 20
 
-shm_distributed/test_runs.py::test_workflow[client_scatter_workflow-plasma] 1.406608s
-Start: 2677 resident, 2428 unique
-End: 3703 resident, 3452 unique
-Δmem: 5
+ plasma client_scatter_workflow
+0.761663s
+Start: 2967 resident, 2623 unique
+End: 3481 resident, 3135 unique
+Δmem: 13
 
-shm_distributed/test_runs.py::test_workflow[client_scatter_workflow-vineyard] 1.543672s
-Start: 3702 resident, 3452 unique
-End: 4727 resident, 4477 unique
-Δmem: 3
-
+ vineyard client_scatter_workflow
+0.844939s
+Start: 3991 resident, 3647 unique
+End: 4504 resident, 4160 unique
+Δmem: 8
 ```
--- shm_distributed/test_runs.py
+++ shm_distributed/test_runs.py
@@ -4,6 +4,7 @@ import psutil
 import pytest
 import time
 
+import dask.dataframe as dd
 import distributed
 
 from shm_distributed.utils import memories
@@ -57,57 +58,73 @@ def test_lmdb_create_many(lmdb_deleter, num):
 
 
 def client_scatter_workflow(client):
-    data = np.empty(2**30, dtype="uint8")  # dropped at the end of this function
+    data = np.empty(2**29, dtype="uint8")  # dropped at the end of this function
     f = client.scatter(data, broadcast=True)  # ephemeral ref in this process, which does not persist
     distributed.wait(f)
+    del data
     return f  # be sure to capture return so that we don't clean up yet
 
 
 def worker_scatter_workflow(client: distributed.Client):
-    f = client.submit(np.empty, 2**30, dtype="uint8")
+    f = client.submit(np.empty, 2**29, dtype="uint8")
     client.replicate(f)
     distributed.wait(f)
-    distributed.wait(client.map(lambda x, _: x[0], [f] * 4, list(range(4))))
+    distributed.wait(client.map(lambda x, _: x[0], [f] * 4, list(range(6))))
     result = f.result()
     return f, result
 
 
-@pytest.mark.parametrize("ser", ["pickle", "lmdb", "plasma", "vineyard"])
-@pytest.mark.parametrize("workflow", [worker_scatter_workflow, client_scatter_workflow])
-def test_workflow(ser, workflow, lmdb_deleter, plasma_session, vineyard_session):
-    client = distributed.Client(
-        n_workers=4,
-        threads_per_worker=1,
-        serializers=[ser, "error"],
-        worker_passthrough=dict(serializers=[ser, "error"]),
+def workflow_dataframe(client):
+    ddf = dd.read_parquet(
+        "/home/mdurant/data/bench/*.parquet",
+        engine="pyarrow",
     )
 
-    # ensure our config got through
-    _ = None
-    try:
-        pids = list(client.run(os.getpid).values()) + [os.getpid()]
-        if ser == "plasma" or ser == "vineyard":
-            for proc in psutil.process_iter():
-                if "plasma" in proc.name().lower() or "vineyard" in proc.name().lower():
-                    pids.append(proc.pid)
-                    break
-        start_mem = memories(pids)
-        mem0 = psutil.virtual_memory().used
-        start_time = time.time()
+    return client.persist(ddf.set_index("id2").rechunk(12).groupby("id1", dropna=False, observed=True
+                                                                   ).agg({"v1": "sum"}))
 
-        _ = workflow(client)
-        time.sleep(1)
 
-        end_mem = memories(pids)
-        mem1 = psutil.virtual_memory().used
-        end_time = time.time() - 1
+@pytest.mark.parametrize("ser", ["pickle", "lmdb", "plasma", "vineyard"])
+@pytest.mark.parametrize("workflow", [workflow_dataframe, worker_scatter_workflow, client_scatter_workflow])
+def test_workflow(ser, workflow, lmdb_deleter, plasma_session, vineyard_session, repeat=2):
+    for _ in range(repeat):
+        client = distributed.Client(
+            n_workers=6,
+            threads_per_worker=1,
+            serializers=[ser, "error"],
+            worker_passthrough=dict(serializers=[ser, "error"]),
+            memory_limit=0
+        )
 
-    finally:
-        del _
+        # ensure our config got through
+        _ = None
+        try:
+            pids = list(client.run(os.getpid).values()) + [os.getpid()]
+            if ser == "plasma" or ser == "vineyard":
+                for proc in psutil.process_iter():
+                    if "plasma" in proc.name().lower() or "vineyard" in proc.name().lower():
+                        print("%%%", proc)
+                        pids.append(proc.pid)
+                        break
+            start_mem = memories(pids)
+            mem0 = psutil.virtual_memory().used
+            start_time = time.time()
 
-        client.close()
+            _ = workflow(client)
+            time.sleep(1)
 
-    print(f"{end_time - start_time:03f}s")
-    print("Start:", round(start_mem["rss"] / 2**20), "resident,", round(start_mem["uss"] / 2**20), "unique")
-    print("End:", round(end_mem["rss"] / 2**20), "resident,", round(end_mem["uss"] / 2**20), "unique")
-    print("Δmem:", round((mem1 - mem0) / 2**20))
+            end_mem = memories(pids)
+            mem1 = psutil.virtual_memory().used
+            end_time = time.time() - 1
+
+        finally:
+            del _
+
+            client.close()
+
+    print("### ", ser, workflow.__name__)
+    print(f"### {end_time - start_time:03f}s")
+    print("### Start:", round(start_mem["rss"] / 2**20), "resident,", round(start_mem["uss"] / 2**20), "unique")
+    print("### End:", round(end_mem["rss"] / 2**20), "resident,", round(end_mem["uss"] / 2**20), "unique")
+    print("### Δmem:", round((mem1 - mem0) / 2**20))
+    print("###")
